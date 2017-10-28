0f77f87dc6dda13ab829f69f10e1cc5c35cba058
Tráng Hà Viết <viettrangha@gmail.com>
2017-10-28 15:50:19 +0700

    add edit search form for candidate

--- resources/views/candidates/index.blade.php
+++ resources/views/candidates/index.blade.php
@@ -1,12 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <h1 class="pull-left">Candidates</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('candidates.create') !!}">Add New</a>
-        </h1>
-    </section>
+    <div class="container-fluid">
+        <section class="content-header row">
+            <h1 class="col-sm-6 pull-left">Candidates</h1>
+            <div class="col-sm-6 pull-right">
+                <a class="btn btn-primary pull-right" style="margin-right: 35px;margin-bottom: 5px" href="{!! route('candidates.create') !!}">New</a>
+            </div>
+        </section>
+        <div class="row">
+            {!! Form::open(['route' => ['candidates.search'], 'method' => 'get']) !!}
+            <div class="form-group col-sm-3">
+                {!! Form::text('q', null, ['class' => 'form-control',
+                'placeholder' => 'Type Author name']) !!}
+            </div>
+            <div class="form-group col-sm-3">
+                {!! Form::number('mutual_authors', null, ['class' => 'form-control',
+                'placeholder' => 'Mutual Author']) !!}
+            </div>
+            <div class="form-group col-sm-3">
+                {!! Form::number('joint_papers', null, ['class' => 'form-control',
+                'placeholder' => 'Joint Papers']) !!}
+            </div>
+            <div class="form-group col-sm-3">
+                {!! Form::number('joint_subjects', null, ['class' => 'form-control',
+                'placeholder' => 'Joint Subjects']) !!}
+            </div>
+            <div class="form-group col-sm-3">
+                {!! Form::number('joint_keywords', null, ['class' => 'form-control',
+                'placeholder' => 'Joint Keywords']) !!}
+            </div>
+            <div class="form-group col-sm-3">
+                {!! Form::number('score_1', null, ['class' => 'form-control',
+                'placeholder' => 'Score 1']) !!}
+            </div>
+            <div class="form-group col-sm-3">
+                {!! Form::number('score_2', null, ['class' => 'form-control',
+                'placeholder' => 'Score 2']) !!}
+            </div>
+            <div class="form-group col-sm-3">
+                {!! Form::number('score_3', null, ['class' => 'form-control',
+                'placeholder' => 'Score 3']) !!}
+            </div>
+            <div class="form-group col-sm-1">
+                {!! Form::submit('Search', ['class' => 'btn btn-primary btn-block']) !!}
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+
     <div class="content">
         <div class="clearfix"></div>
 
@@ -21,4 +63,6 @@
         </div>
     </div>
 @endsection
+
+
 
