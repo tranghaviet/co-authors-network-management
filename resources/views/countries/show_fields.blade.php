2422ac483801519a932979e25ae5acbf494f81c2
Tráng Hà Viết <viettrangha@gmail.com>
2017-10-30 19:37:57 +0700

    add more info for university, city, country show view

--- resources/views/countries/show_fields.blade.php
+++ resources/views/countries/show_fields.blade.php
@@ -10,3 +10,14 @@
     <p>{!! $country->name !!}</p>
 </div>
 
+<!-- Cities Field -->
+<div class="form-group">
+    {!! Form::label('cities', 'Cities:') !!}
+    <ol>
+        @foreach($country->cities as $city)
+            <li>
+                <a href="{!! route('cities.show', [$city['id']]) !!}">{!! $city['name'] !!}</a>
+            </li>
+        @endforeach
+    </ol>
+</div>
