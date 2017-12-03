c22d9e5e21ef4c98706b1e23bbc12cef3d0b9aec
Tráng Hà Viết <viettrangha@gmail.com>
2017-12-03 18:19:50 +0700

    add measure linking

--- app/Models/CoAuthor.php
+++ app/Models/CoAuthor.php
@@ -86,6 +86,14 @@ class CoAuthor extends Model
     }
 
     /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     **/
+    public function coAuthorPaper()
+    {
+        return $this->hasMany(\App\Models\CoAuthorPaper::class);
+    }
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      **/
     public function papers()
@@ -127,5 +135,73 @@ class CoAuthor extends Model
             ->first(['no_of_joint_papers'])['no_of_joint_papers'];
 
         return $result ?: 0;
+    }
+
+    /**
+     * Get co-author with their info in authors table.
+     *
+     * @param float $authorId author id
+     * @param array $columns fields to get
+     * @return \Illuminate\Support\Collection|static
+     */
+    public static function collaborators($authorId, $columns = ['*'])
+    {
+        $coAuthors = self::coAuthors($authorId, ['first_author_id', 'second_author_id',]);
+
+        $collaboratorIds = [];
+
+        foreach ($coAuthors as $coAuthor) {
+            if ($coAuthor->first_author_id == $authorId) {
+                array_push($collaboratorIds, $coAuthor->second_author_id);
+            } else {
+                array_push($collaboratorIds, $coAuthor->first_author_id);
+            }
+        }
+
+        return Author::whereIn('id', $collaboratorIds)->get($columns);
+    }
+
+    /**
+     * Get co-author with info in co_authors table.
+     *
+     * @param float $authorId author id
+     * @param array $columns fields to get
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public static function coAuthors($authorId, $columns = ['*'])
+    {
+        return self::where([
+            ['first_author_id', '=', $authorId],
+            ['no_of_joint_papers', '>', 0],
+        ])->orWhere([
+            ['second_author_id', '=', $authorId],
+            ['no_of_joint_papers', '>', 0],
+        ])->get($columns);
+    }
+
+    /**
+     * Get co-author with some following info: id, no_of_joint_papers, no_of_mutual_authors, no_of_joint_subjects, no_of_joint_keywords.
+     *
+     * @param float $authorId author id
+     * @param array $columns fields to get
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public static function coAuthorWithoutInfo($authorId, $columns = ['*'])
+    {
+        $coAuthors = self::coAuthors($authorId, $columns);
+
+        if (! empty($coAuthors)) {
+            for ($i = 0; $i < $coAuthors->count(); $i++) {
+                if ($coAuthors[$i]->first_author_id == $authorId) {
+                    $coAuthors[$i]['id'] = $coAuthors[$i]->first_author_id;
+                    unset($coAuthors[$i]->first_author_id);
+                } else {
+                    $coAuthors[$i]['id'] = $coAuthors[$i]->second_author_id;
+                    unset($coAuthors[$i]->second_author_id);
+                }
+            }
+        }
+
+        return $coAuthors;
     }
 }
