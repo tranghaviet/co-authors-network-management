de22ff4c3bb073b45f08b9aaaf2ecc547f5cc92c
Tráng Hà Viết <viettrangha@gmail.com>
2017-12-08 15:12:50 +0700

    update papers view

--- resources/views/papers/show.blade.php
+++ resources/views/papers/show.blade.php
@@ -11,7 +11,7 @@
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
                     @include('papers.show_fields')
-                    <a href="{!! route('papers.index') !!}" class="btn btn-default">Back</a>
+                    <a href="{!! route($routeType . 'papers.index') !!}" class="btn btn-default">Back</a>
                 </div>
             </div>
         </div>
